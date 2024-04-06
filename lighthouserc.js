369180de548b966e2f14e27c2f3ab5d21fddada1
talitasantos2310@gmail.com <Maycon@1802>
2024-04-06 10:14:36 -0300

    atualizacao do lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,18 +1,18 @@
 module.exports = {
     ci: {
-      collect: {
-        url: ['https://www.businessinsider.com/'], 
-        numberOfRuns: 3, 
-      },
-      assert: {
-        assertions: {
-          'accessibility': ['error', { 
-            'minScore': 0.8, 
-          }],
+        collect: {
+            url: ['https://www.businessinsider.com/'],
+            numberOfRuns: 3
         },
-      },
-      upload: {
-        target: 'temporary-public-storage', 
-      },
-    },
-  };
+        assert: {
+            "assert": {
+                "assertions": {
+                    "categories:accessibility": ["error", {"minScore": 0.82}]
+                }
+            }
+        },
+        upload: {
+            target: 'temporary-public-storage',
+        }
+    }
+};
